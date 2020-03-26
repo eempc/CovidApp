bb10301f82ba62292c5098325aaebf7a19d51f1d
eempc <evc484@gmail.com>
2020-03-26 13:58:48 +0000

    Google Geocharts

--- CovidApp/Pages/Map.cshtml.cs
+++ CovidApp/Pages/Map.cshtml.cs
@@ -13,10 +13,9 @@ namespace CovidApp
     public class MapModel : PageModel
     {
         private readonly CovidApp.Data.CovidAppContext _context;
-        private readonly IWebHostEnvironment _env;
-
-        public string testDir;
-        public IEnumerable<string> contents;
+        //private readonly IWebHostEnvironment _env;
+        //public string testDir;
+        //public IEnumerable<string> contents;
 
         public IList<AccessionRecord> Records { get; set; }
         public AccessionRecord ReferenceStrain { get; set; }
@@ -24,9 +23,9 @@ namespace CovidApp
         public MapModel(CovidApp.Data.CovidAppContext context, IWebHostEnvironment env)
         {
             _context = context;
-            _env = env;
-            testDir = _env.ContentRootPath;
-            contents = Directory.EnumerateDirectories(testDir);
+            //_env = env;
+            //testDir = _env.ContentRootPath;
+            //contents = Directory.EnumerateDirectories(testDir);
         }
 
         public void OnGet()
