9a68cd5e98cb0e0631a9640b46c32a38feeb08c1
eempc <evc484@gmail.com>
2020-03-26 12:19:45 +0000

    ctor with context

---
+++ CovidApp/Pages/Map.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CovidApp
+{
+    public class MapModel : PageModel
+    {
+        private readonly CovidApp.Data.CovidAppContext _context;
+
+        public MapModel(CovidApp.Data.CovidAppContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
