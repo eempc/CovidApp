05c2888f99419ed70bf1acb3faa54af9cf4351d9
eempc <evc484@gmail.com>
2020-03-26 13:51:37 +0000

    deviations calculated

--- CovidApp/Pages/Map.cshtml.cs
+++ CovidApp/Pages/Map.cshtml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CovidApp.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -10,15 +13,26 @@ namespace CovidApp
     public class MapModel : PageModel
     {
         private readonly CovidApp.Data.CovidAppContext _context;
+        private readonly IWebHostEnvironment _env;
 
-        public MapModel(CovidApp.Data.CovidAppContext context)
+        public string testDir;
+        public IEnumerable<string> contents;
+
+        public IList<AccessionRecord> Records { get; set; }
+        public AccessionRecord ReferenceStrain { get; set; }
+
+        public MapModel(CovidApp.Data.CovidAppContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
+            testDir = _env.ContentRootPath;
+            contents = Directory.EnumerateDirectories(testDir);
         }
 
         public void OnGet()
         {
-
+            Records = _context.AccessionRecord.ToList();
+            ReferenceStrain = Records.Single(x => x.Id == -1);
         }
     }
 }
